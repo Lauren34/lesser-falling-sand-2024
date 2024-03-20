7b66f66340341802d8ca07c2575fb2e7c0c320d3
ilana lesser <llesser@student.touro.edu>
2024-03-20 10:28:06 -0700

    trying this

--- src/main/java/lesser/fallingsand/Sand.java
+++ src/main/java/lesser/fallingsand/Sand.java
@@ -1,29 +1,37 @@
 package lesser.fallingsand;
 
-public class Sand {
+import java.util.Random;
 
-    private int[][] field = new int[3][3];
 
+class Sand {
+    private final int[][] field;
+    private final Random random;
+
+    public Sand(int width, int height) {
+        field = new int[height][width];
+        random = new Random();
+    }
+
+    public Sand(int width, int height, Random random) {
+        field = new int[height][width];
+        this.random = random;
+    }
     public String toString() {
         StringBuilder builder = new StringBuilder();
-
         for (int y = 0; y < 3; y++) {
             for (int x = 0; x < 3; x++) {
                 builder.append(field[y][x]);
             }
             builder.append("\n");
         }
-
         return builder.toString();
     }
-
     /**
      * @return the value in field
      */
     public int get(int x, int y) {
         return field[y][x];
     }
-
     /**
      * Sets the value in field to be 1
      */
@@ -31,22 +39,38 @@ public class Sand {
         field[y][x] = 1;
     }
 
-
-
+    public void randomSand(int n) {
+        for (int i = 0; i < n; i++) {
+            int x = random.nextInt(field[0].length);
+            int y = random.nextInt(field.length);
+            field[y][x] = 1;
+        }
+    }
 
     public void fall() {
-        for (int y = field.length - 2; y >= 0; y--){
-            for (int x = 0; x < field.length; x++){
-                if (field[y][x] == 1 && field[y + 1][x] == 0){
-                    if (y < field.length - 1 && field[y + 1][x] == 0){
-                        field[y + 1][x] = 1;
-                        field[y][x] = 0;
-
-
+        for (int y = field.length - 2; y >= 0; y--) {
+            for (int x = 0; x < field[y].length; x++) {
+                if (field[y][x] == 1) {
+                    if (y + 1 < field.length) {
+                        if (field[y + 1][x] == 0) {
+                            field[y][x] = 0;
+                            field[y + 1][x] = 1;
+                        } else if (random.nextBoolean()) {
+                            if (x + 1 < field[y].length && field[y + 1][x + 1] == 0) {
+                                field[y][x] = 0;
+                                field[y + 1][x + 1] = 1;
+                            }
+                        } else {
+                            if (x - 1 >= 0 && field[y + 1][x - 1] == 0) {
+                                field[y][x] = 0;
+                                field[y + 1][x - 1] = 1;
+                            }
+                        }
                     }
                 }
             }
         }
     }
 }
+
 
--- src/test/java/lesser/fallingsand/SandTest.java
+++ src/test/java/lesser/fallingsand/SandTest.java
@@ -2,65 +2,32 @@ package lesser.fallingsand;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 class SandTest {
 
     @Test
-    public void string() {
-        // given - set up paramaters
-        Sand sand = new Sand();
-
-        // when - the one thing you are testing gets tested
-        String actual = sand.toString();
-
-        // then - check results
-        assertEquals("000\n000\n000\n", actual);
-    }
-
-    @Test
-    public void put() {
+    public void fallToTheRight() {
         // given
-        Sand sand = new Sand();
-
-        // when
-        sand.put(1, 0);
-
-        // then
-        assertEquals("010\n000\n000\n", sand.toString());
-    }
-
-
-
-    @Test
-    public void fall() {
-        // given
-        Sand sand = new Sand();
+        Sand sand = new Sand(3, 3);
+        sand.put(1, 1);
         sand.put(1, 0);
 
         // when
         sand.fall();
 
         // then
-        assertEquals("000\n010\n000\n", sand.toString());
+        assertEquals("000\n000\n011\n", sand.toString());
     }
 
     @Test
-    public void fallOnGround() {
+    public void fallToTheLeft() {
         // given
-        Sand sand = new Sand();
-        sand.put(1, 2);
-
-        // when
-        sand.fall();
-
-        // then
-        assertEquals("000\n000\n010\n", sand.toString());
-    }
-    @Test
-    public void fallOnOtherSand() {
-        // given
-        Sand sand = new Sand();
+        Sand sand = new Sand(3, 3);
         sand.put(1, 1);
         sand.put(1, 2);
 
@@ -68,6 +35,43 @@ class SandTest {
         sand.fall();
 
         // then
-        assertEquals("000\n010\n010\n", sand.toString());
+        assertEquals("000\n000\n110\n", sand.toString());
+    }
+
+    @Test
+    public void fallRandomDirectionRight() {
+        // given
+        Random random = mock(Random.class);
+        doReturn(true).when(random).nextBoolean();
+        Sand sand = new Sand(3, 3, random);
+        sand.put(1, 1);
+        sand.put(1, 2);
+
+        // when
+        sand.fall();
+
+        // then
+        assertEquals("000\n000\n011\n", sand.toString());
+    }
+
+    @Test
+    public void fallRandomDirectionLeft() {
+        // given
+        Random random = mock(Random.class);
+        doReturn(false).when(random).nextBoolean();
+        Sand sand = new Sand(3, 3, random);
+        sand.put(1, 1);
+        sand.put(1, 2);
+
+        // when
+        sand.fall();
+
+        // then
+        assertEquals("000\n000\n110\n", sand.toString());
+    }
+
+    @Test
+    public void fallSimultaneously() {
+        // given
     }
 }
